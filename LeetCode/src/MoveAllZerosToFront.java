9b220b9aeaa7afdfd2582550f5e162226ead42dc
santhosh947 <santhosh.cholleti47@gmail.com>
2017-09-01 11:16:58 -0500

    Added tic-tac-toe problem

--- LeetCode/src/MoveAllZerosToFront.java
+++ LeetCode/src/MoveAllZerosToFront.java
@@ -1,12 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoveAllZerosToFront {
 	public static void main(String[] args) {
-		int[] arr= {1,2,3,0,0,0,4,5};
-		moveZeroes_tolast(arr);
+		int[] arr= {0,2,0,0,0,0,0,4,5};
+		moveZeros_Front(arr);
 		for(int i: arr)
 			System.out.print(i + " ");
 	}
-
+	
+	public static void moveZeros_Front(int[] nums)
+	{
+		if(nums==null || nums.length ==0)
+			return;
+		
+		int c=0, len=0;
+		List<Integer> pos=new ArrayList<Integer>();
+		for(int i=0;i<nums.length;i++)
+		{
+			if(nums[i]==0)
+			{
+				len++;
+				c++;
+				pos.add(i);
+			}
+			
+		}
+		
+		for(int i: pos)
+		{
+			if(i<c)
+			{
+				c--;
+			}
+		}
+		
+		int[] temp=new int[c];
+		int ct=0;
+		for(int i=0;i<nums.length;i++)
+		{
+			if(nums[i]!=0 && ct<c)
+				temp[ct++]=nums[i];
+		}
+		
+		for(int i=0;i<len;i++)
+		{
+			nums[i]=0;
+		}
+		
+		ct=0;
+		for(int i=len;i<len+c;i++)
+		{
+			nums[i]=temp[ct++];
+		}
+		
+	}
+	
 	public static void moveZeroes_tolast(int[] nums) {
 	    if (nums == null || nums.length == 0) return;        
 
@@ -20,34 +69,4 @@ public class MoveAllZerosToFront {
 	    }
 	}
 	
-	private static void moveZeros(int[] arr) {
-		int c=0;
-		for(int i=0;i<arr.length;i++)
-		{
-			if(arr[i]==0)
-				c++;
-		}
-		int[] temp=new int[c];
-		int it=0;
-		int ct=0;
-		for(int i=0;i<arr.length;i++)
-		{
-			if(ct>c-1)
-				ct=0;
-			
-			if( it<c && arr[i]!=0)
-			{
-				temp[it]=arr[i];
-				it++;
-				arr[i]=0;
-			}
-			else{
-				int t=arr[i];
-				arr[i]=temp[ct];
-				temp[ct]=t;
-				ct++;
-			}
-		}
-		
-	}
 }
---
+++ LeetCode/src/PlayTicTacToe.java
@@ -0,0 +1,14 @@
+
+public class PlayTicTacToe {
+	public static void main(String[] args) {
+		TicTacToe t= new TicTacToe(3);
+		System.out.println(t.move(1,1,1));
+		System.out.println(t.move(1,2,2));
+		System.out.println(t.move(0,0,1));
+		System.out.println(t.move(2,2,2));
+		System.out.println(t.move(0,2,1));
+		System.out.println(t.move(0,1,2));
+		System.out.println(t.move(2,0,1));
+
+	}
+}

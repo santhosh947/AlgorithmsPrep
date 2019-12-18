bca5b0e7ceb5987150607da7a1023249b2c87315
Santhosh Cholletisasasss <sacholle@microsoft.com>
2019-12-17 22:12:13 -0800

    Added Linkedlist

--- src/Practice_Nov_2019/CopyRandomLinkedList.java
+++ src/Practice_Nov_2019/CopyRandomLinkedList.java
@@ -61,3 +61,4 @@ public class CopyRandomLinkedList {
         return res;
     }
 }
+}
--- src/Practice_Nov_2019/ReorderLinkedList.java
+++ src/Practice_Nov_2019/ReorderLinkedList.java
@@ -5,36 +5,39 @@ import java.util.*;
 /**
  * Given 1->2->3->4, reorder it to 1->4->2->3.
  * 
+ * 1,2,3,4
+ * 4,3,2,1
+ * 
  * Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
  */
-class ReorderLinkedList
-{
+class ReorderLinkedList {
     public static void main(String[] args) {
         ListNode a0 = new ListNode(1);
         ListNode a1 = new ListNode(2);
         ListNode a2 = new ListNode(3);
         ListNode a3 = new ListNode(4);
+        ListNode a4 = new ListNode(5);
+
 
         a0.next = a1;
         a1.next = a2;
         a2.next = a3;
+      //  a3.next = a4;
 
-        ListNode t = a1;
-        while(t!=null)
-        {
-            System.out.print(t.val +"->");
-            t=t.next;
+        ListNode t = a0;
+        while (t != null) {
+            System.out.print(t.val + "->");
+            t = t.next;
         }
 
         System.out.println();
 
-        reorderList(a1);
+        reorderList(a0);
 
-        t = a1;
-        while(t!=null)
-        {
-            System.out.print(t.val +"->");
-            t=t.next;
+        t = a0;
+        while (t != null) {
+            System.out.print(t.val + "->");
+            t = t.next;
         }
 
         System.out.println();
@@ -42,29 +45,31 @@ class ReorderLinkedList
 
     public static void reorderList(ListNode head) {
         ListNode t_h = head;
-        if(head == null)
+        if (head == null || head.next==null)
             return;
-        Map<Integer, ListNode> st = new HashMap<Integer, ListNode>();
+        Stack<ListNode> st = new Stack();
         int ct = 0;
-        while(t_h!=null)
-        {
-            st.put(ct, t_h);
+        while (t_h != null) {
+            st.add(t_h);
             t_h = t_h.next;
             ct++;
         }
 
-        int start = 0;
-        ListNode  curr = head;
-        ListNode prev =curr;
-        while(start<ct)
-        {
-            ListNode temp = curr.next;
-            t_h.next = st.get(--ct);
-            start++;
-            t_h= t_h.next;
-            t_h.next=temp;
+        int start = 1;
+        t_h = head;
+        while (start < ct) {
+            ListNode temp = t_h.next;
+            t_h.next = st.pop();
             t_h = t_h.next;
+            t_h.next = temp;
+            t_h = t_h.next;
+            start += 2;
         }
-
-    }
+        if (t_h.next != null) {
+            if(ct%2==0)
+                t_h.next.next = null;
+            else   
+                t_h.next.next.next = null;
+        }
+     }
 }
